d1aeabf46d92a8fe865d388cdcfed60f12a104fa
pratamamp <pratamamp@gmail.com>
2023-02-23 06:49:45 +0700

    updates

--- api-cms/src/api/handling/controllers/handling.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * handling controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::handling.handling');
--- api-cms/src/api/handling/services/handling.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * handling service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::handling.handling');
